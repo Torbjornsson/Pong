61179e118d12423b1e1821d32a5b7e9e72e261a4
Torbjornsson <david.torbjornsson@gmail.com>
2020-10-03 18:33:17 +0200

    Adds unit test project and first test class

---
+++ Pong.UnitTests/Player.UnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pong.UnitTests
+{
+    [TestClass]
+    public class Player_UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
